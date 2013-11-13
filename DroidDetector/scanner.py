7e2700b6ecbe1f0433d6c06154bf1f2b0150a280
Richard Yeh <apocryphon.now@gmail.com>
2013-11-12 21:10:49 -0800

    store Permissions alphabetically, filters out dummy methods

--- DroidDetector/scanner.py
+++ DroidDetector/scanner.py
@@ -17,7 +17,24 @@ def main(argv):
     args = parser.parse_args()
     apk_file = args.apk
     a, d, dx = AnalyzeAPK(apk_file, decompiler="DAD")
-    show_Permissions(dx)
+
+    perms_list = dx.get_permissions( [] ).keys()
+    perms_list.sort()
+    print "Permissions requested by app are: ", perms_list
+
+    alphabet_list = string.ascii_letters
+
+    #alphabet_list = []
+    #alphabet_list.append(string.uppercase)
+    #alphabet_list.append(string.lowercase)
+    #print alphabet_list
+
+    methods_list = d.get_methods()
+    for method in methods_list:
+        if method.get_name() not in alphabet_list:
+            print "Name: ", method.get_name()
+            #print method.show_info()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
